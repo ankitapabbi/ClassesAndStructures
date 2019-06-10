ad10b5501126e87c6596e54b012ef3df1990018f
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-10 19:33:13 -0400

    Initial Commit

---
+++ ClassesAndStructures/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  ClassesAndStructures
+//
+//  Created by MacStudent on 2019-06-10.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+// creating obj of the class Student
+var s1 = Student() // calling the constructers
+
+s1.dislay()
+s1.setValues()
+s1.dislay()
+
+s1.printData()
+
+print(s1.getFirstName())
+
+var s2: Student
+
+s2 = Student()
